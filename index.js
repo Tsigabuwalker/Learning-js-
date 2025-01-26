399901254256f04435c54c9438842431a82c6244
Tsigabuwalker <seyoumwalker@gmail.com>
2025-01-25 20:38:13 -1000

    3rd commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let name ="Tsigabu Seyoum";
 console.log(name);
 // reserved variable can not be as variables like if ,for , else etc
+let interestRate = 0.3;
+interestRate = 3;
+console.log(interestRate);
 
 
