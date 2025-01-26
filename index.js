57c7ed8e29273c085b2c95ad3620a3cc8cc2498c
Tsigabuwalker <seyoumwalker@gmail.com>
2025-01-25 20:57:58 -1000

    4th commit

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ console.log(name);
 let interestRate = 0.3;
 interestRate = 3;
 console.log(interestRate);
+// What makes js especial for the other languages is dynamic typing not static
+let fam = "mosh";
+console.log(typeof(fam));
 
-
+let person = {
+    name: "Abel",
+    age: 23,
+    cgpa: 3.5,
+    department: "computer Science"
+};
+console.log(person);
+console.log(person.name);
+console.log(person.cgpa);
+console.log(person.department);
